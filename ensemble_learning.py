4d369fac80eb678cc92fc096c4872c9afd3620ae
prateep.mukherjee <mukherjeeprateep@gmail.com>
2018-09-10 21:04:59 +0530

    - gradient boosting included

--- ensemble_learning.py
+++ ensemble_learning.py
@@ -2,8 +2,9 @@ import numpy as np
 from sklearn.datasets import make_moons
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import StandardScaler
-
 from matplotlib import pyplot as plt
+
+CHAPTER_ID = "ensembles"
 
 X, y = make_moons(n_samples=500, noise=0.3, random_state=42)
 X = StandardScaler().fit_transform(X)
@@ -75,11 +76,11 @@ def bagging_pasting(X_train, X_val, y_train, y_val, doplot=False):
         plt.subplot(122)
         plot_decision_boundary(bag_clf, X, y)
         plt.title("Decision Trees with Bagging", fontsize=14)
-        save_fig("DT_without_and_with_bagging_plot", "ensembles")
+        save_fig("DT_without_and_with_bagging_plot", CHAPTER_ID)
         plt.show()
 
 
-bagging_pasting(X_train, X_val, y_train, y_val, doplot=True)
+# bagging_pasting(X_train, X_val, y_train, y_val, doplot=True)
 
 """
 Adaboost:
@@ -103,7 +104,7 @@ Adaboost:
         \hat{y}(x) = argmax_k \sum_{i=1}^{N}, \hat{y_j}^{(i)} = k \alpha_j 
 """
 
-def adaboost(X_train, X_val, y_train, y_val):
+def adaboost(X_train, X_val, y_train, y_val, doplot=False):
     from sklearn.ensemble import AdaBoostClassifier
     from sklearn.tree import DecisionTreeClassifier
     from sklearn.metrics import accuracy_score
@@ -114,5 +115,117 @@ def adaboost(X_train, X_val, y_train, y_val):
     y_pred = ada_clf.predict(X_val)
     print("Adaboost classifier, accuracy score = %f\n" % accuracy_score(y_val, y_pred))
 
-# adaboost(X_train, X_val, y_train, y_val)
+    if doplot:
+        from utils import plot_decision_boundary, save_fig
+        plt.figure(figsize=(11, 4))
+        plot_decision_boundary(ada_clf, X, y)
+        plt.title("Adaboost classifcation with Decision Tree base estimator", fontsize=12)
+        save_fig("AdaBoost_with_DT", CHAPTER_ID)
+        plt.show()
 
+# adaboost(X_train, X_val, y_train, y_val, doplot=True)
+
+
+"""
+Gradient Boosting(GB)
+"""
+
+def gb_intuition(doplot=False):
+    np.random.seed(42)
+    X = np.random.rand(100, 1) - 0.5
+    y = 3 * X[:, 0]**2 + 0.05 * np.random.randn(100)
+
+    from sklearn.tree import DecisionTreeRegressor
+    from utils import plot_predictions, save_fig
+
+    tree_reg1 = DecisionTreeRegressor(max_depth=5, random_state=42)
+    tree_reg1.fit(X, y)
+
+    y2 = y - tree_reg1.predict(X)
+    tree_reg2 = DecisionTreeRegressor(max_depth=5, random_state=42)
+    tree_reg2.fit(X, y2)
+
+    y3 = y2 - tree_reg2.predict(X)
+    tree_reg3 = DecisionTreeRegressor(max_depth=5, random_state=42)
+    tree_reg3.fit(X, y3)
+
+    X_new = np.array([[0.8]])
+
+    y_pred = sum(tree.predict(X_new) for tree in (tree_reg1, tree_reg2, tree_reg3))
+    print("Prediction of 3 trees ", y_pred)
+
+    if doplot:
+        plt.figure(figsize=(11,11))
+
+        plt.subplot(321)
+        plot_predictions([tree_reg1], X, y, axes=[-0.5, 0.5, -0.1, 0.8],
+                         label="$h_1(x_1)$", style='g-',
+                         data_label='Training set')
+        plt.ylabel("$y$", fontsize=16, rotation=0)
+        plt.title("Residuals and tree predictions", fontsize=16)
+
+        plt.subplot(322)
+        plot_predictions([tree_reg1], X, y, axes=[-0.5, 0.5, -0.1, 0.8],
+                         label="$h(x_1) = h_1(x_1)$",
+                         data_label='Training set')
+        plt.ylabel("$y$", fontsize=16, rotation=0)
+        plt.title("Ensemble predictions", fontsize=16)
+
+        plt.subplot(323)
+        plot_predictions([tree_reg2], X, y2, axes=[-0.5, 0.5, -0.1, 0.8],
+                         label="$h_2(x_1)$", style='g-',
+                         data_style="k+", data_label='Residuals')
+        plt.ylabel("$y - h_1(x_1)$", fontsize=16, rotation=0)
+
+        plt.subplot(324)
+        plot_predictions([tree_reg1, tree_reg2], X, y, axes=[-0.5, 0.5, -0.1, 0.8],
+                         label="$h(x_1) = h_1(x_1) + h_2(x_1)$")
+        plt.ylabel("$y$", fontsize=16, rotation=0)
+
+        plt.subplot(325)
+        plot_predictions([tree_reg3], X, y3, axes=[-0.5, 0.5, -0.1, 0.8],
+                         label="$h_3(x_1)$", style='g-',
+                         data_style="k+")
+        plt.ylabel("$y - h_1(x_1) - h_2(x_1)$", fontsize=16, rotation=0)
+        plt.xlabel("$x_1", fontsize=16)
+
+        plt.subplot(326)
+        plot_predictions([tree_reg1, tree_reg2, tree_reg3], X, y, axes=[-0.5, 0.5, -0.1, 0.8],
+                         label="$h(x_1) = h_1(x_1) + h_2(x_1) + h_3(x_1)$")
+        plt.ylabel("$y$", fontsize=16, rotation=0)
+        plt.xlabel("$x_1", fontsize=16)
+
+        save_fig("gradient_boosting_plot", CHAPTER_ID)
+        plt.show()
+
+# gb_intuition(doplot=True)
+
+def gradient_boosting(doplot=False):
+    from sklearn.ensemble import GradientBoostingRegressor
+    from utils import plot_predictions, save_fig
+
+    np.random.seed(42)
+    X = np.random.rand(100, 1) - 0.5
+    y = 3 * X[:, 0] ** 2 + 0.05 * np.random.randn(100)
+
+    gbrt = GradientBoostingRegressor(max_depth=2, n_estimators=3, learning_rate=1.0)
+    gbrt.fit(X, y)
+
+    gbrt_slow = GradientBoostingRegressor(max_depth=2, n_estimators=200, learning_rate=0.1)
+    gbrt_slow.fit(X, y)
+
+    if doplot:
+        plt.figure(figsize=(11,4))
+
+        plt.subplot(121)
+        plot_predictions([gbrt], X, y, axes=[-0.5, 0.5, -0.1, 0.8], label="Ensemble predictions")
+        plt.title("learning_rate = {}, n_estimators = {}".format(gbrt.learning_rate, gbrt.n_estimators))
+
+        plt.subplot(122)
+        plot_predictions([gbrt_slow], X, y, axes=[-0.5, 0.5, -0.1, 0.8], label="Ensemble predictions")
+        plt.title("learning_rate = {}, n_estimators = {}".format(gbrt.learning_rate, gbrt.n_estimators))
+
+        save_fig("gbrt_learning_rate_plot", CHAPTER_ID)
+        plt.show()
+
+gradient_boosting(doplot=True)
--- utils.py
+++ utils.py
@@ -39,3 +39,12 @@ def checkNcreateFolder1(path, folder):
     except OSError as excep:
         if excep.errno != errno.EXIST:
             raise
+
+def plot_predictions(regressors, X, y, axes, label=None, style="r-", data_style="b.", data_label=None):
+    x1 = np.linspace(axes[0], axes[1], 500)
+    y_pred = sum(regressor.predict(x1.reshape(-1, 1)) for regressor in regressors)
+    plt.plot(X[:, 0], y, data_style, label=data_label)
+    plt.plot(x1, y_pred, style, linewidth=2, label=label)
+    if label or data_label:
+        plt.legend(loc="upper center", fontsize=16)
+    plt.axis(axes)
